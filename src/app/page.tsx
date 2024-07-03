8750b2bcff096692f39657bf5a7f99c4480762b4
RichGod Usen <usenrichgod93@gmail.com>
2024-07-03 19:31:08 +0100

    WIP: imported Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image'
+import CustomButton from '@/components/ui/Button/CustomButton';
+import Hero from '@/components/ui/Hero';
+import { Orbitron } from 'next/font/google';
+import Image from 'next/image';
+
+const orbitron = Orbitron({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className=""></main>
+  return (
+    <main className="">
+      {/* Hero Section */}
+      <Hero headline='Discover StudioX Collections' />
+
+    </main>
+  );
 }
