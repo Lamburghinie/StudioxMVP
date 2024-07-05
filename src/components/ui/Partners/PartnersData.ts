82f8e04bb845179a6aa8c7505a4cedd25219dd48
RichGod Usen <usenrichgod93@gmail.com>
2024-07-05 13:53:15 +0100

    WIP: create partners component

---
+++ src/components/ui/Partners/PartnersData.ts
@@ -0,0 +1,35 @@
+/** Images */
+export const partnerImages = [
+  {
+    path: '/assets/partner-icons/ikea.svg',
+    alt: 'ikea logo',
+  },
+  {
+    path: '/assets/partner-icons/walmart.svg',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/amazon-alexa.svg',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/apple-home.png',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/tuya.svg',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/starlink.svg',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/group.svg',
+    alt: '',
+  },
+  {
+    path: '/assets/partner-icons/airbnb.svg',
+    alt: '',
+  },
+]
