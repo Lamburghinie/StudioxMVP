a61e0b8d7a32a7c0c19e9fdaffe4ede45e58068d
RichGod Usen <usenrichgod93@gmail.com>
2024-07-06 12:36:28 +0100

    updated hero component

--- src/components/ui/Hero/Hero.tsx
+++ src/components/ui/Hero/Hero.tsx
@@ -3,6 +3,8 @@ import s from './Hero.module.css';
 import { Orbitron } from 'next/font/google';
 import Image from 'next/image';
 import CustomButton from '../Button/CustomButton';
+import Grid from '../Grid';
+import { heroImages } from './HeroData';
 
 const orbitron = Orbitron({ subsets: ['latin'] });
 
@@ -19,31 +21,25 @@ const Hero: FC<HeroProps> = ({ headline, description }) => {
                 <h1 className={`${orbitron.className} ${s.title}`}>{headline}</h1>
                 <p>{description}</p>
             </div>
-            <div className="w-fit grid grid-cols-2 grid-rows-2 gap-3">
-                <Image
-                    src="/assets/home-banner-image-0.png"
-                    alt="banner image"
-                    width={600}
-                    height={200}
-                    priority
-                    className='row-span-2'
-                />
-                <Image
-                    src="/assets/home-banner-image-1.png"
-                    alt="banner image"
-                    width={600}
-                    height={100}
-                    priority
-                />
-                <Image
-                    src="/assets/home-banner-image-2.png"
-                    alt="banner image"
-                    width={600}
-                    height={100}
-                    priority
-                />
+            <div className='flex items-center justify-center'>
+                <Grid layout='A'>
+                    {
+                        heroImages?.map((val, i) => (
+                            <Image
+                                src={val.path}
+                                alt={val.alt}
+                                key={i}
+                                width={val.width}
+                                height={val.height}
+                                priority
+                            />
+                        ))
+                    }
+                </Grid>
             </div>
-            <CustomButton>
+
+
+            <CustomButton className={s.button}>
                 Buy Now
             </CustomButton>
         </section>
